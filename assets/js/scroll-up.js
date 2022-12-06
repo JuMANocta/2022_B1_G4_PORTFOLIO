32bc0fece33bd8fff7d49e1c74a865ed0edb1092
dev <dev@dev.dev>
2022-12-06 11:57:17 +0100

    ajout du scroll up

---
+++ assets/js/scroll-up.js
@@ -0,0 +1,10 @@
+const scrollUp = document.querySelector('.scroll-up');
+
+scrollUp.addEventListener('click', () => {
+    console.log("scrool up");
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+    });
+});
