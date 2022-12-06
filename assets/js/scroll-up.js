0899144791257164ca7dc194051fcacf3679d47f
dev <dev@dev.dev>
2022-12-06 12:10:25 +0100

    ajout du buger menu

--- assets/js/scroll-up.js
+++ assets/js/scroll-up.js
@@ -8,3 +8,11 @@ scrollUp.addEventListener('click', () => {
         behavior: 'smooth'
     });
 });
+
+const burger = document.querySelector('.burger-menu');
+const ul = document.querySelector('nav ul');
+const nav = document.querySelector('nav');
+
+burger.addEventListener('click', () => {
+    ul.classList.toggle('show');
+});
